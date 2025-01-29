257addda439a1b251dda27f0a25da12899be1a14
ImranPatel <140828177+imran95patel@users.noreply.github.com>
2025-01-29 16:25:05 +0530

    Secont commit

--- src/components/Example.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Example = (prop) => {
-  return <div></div>;
-};
-
-export default Example;
---
+++ src/components/FIrst.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FIrst = () => {
+  return <div>THis is my first component commentn</div>;
+};
+
+export default FIrst;

d3da9dfc809d8e878460a7501414e503da8de53a
ImranPatel <140828177+imran95patel@users.noreply.github.com>
2025-01-29 15:42:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>Home PAge</div>;
+};
+
+export default App;
---
+++ src/components/Example.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Example = (prop) => {
+  return <div></div>;
+};
+
+export default Example;
